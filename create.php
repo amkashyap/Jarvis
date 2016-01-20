8d9143079c640d8c521399c2bdcf5ad0a7d34c25
Amit Kashyap <amit.tech100@gmail.com>
2016-01-20 14:22:14 +0530

    Commiting the create code

---
+++ create.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'connection.php';
+
+$a = $_POST['assignee'];
+$j = $_POST['jiraid'];
+$r = $_POST['regression'];
+$i = $_POST['instructions'];
+
+if(!$_POST['submit']) {
+ echo "please fill out the form";
+  header('Location: ca.html');
+}
+else {
+
+       mysql_query("INSERT INTO daily_assign(`Assignee`,`JiraID`,`Regression`,`Instructions`) VALUES('$a','$j','$r','$i')") or die(mysql_error());
+
+           echo "Data saved into database successfully ";
+           header('Location: ca.html');
+  }
+?>
+
